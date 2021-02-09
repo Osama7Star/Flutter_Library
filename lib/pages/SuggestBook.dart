3d6ae6843a07f0927cf7361aec64b5735c389c42
OsamaAlnajm <56093809+OsamaAlnajm@users.noreply.github.com>
2021-02-09 18:50:44 +0300

    5. Commit

--- lib/pages/SuggestBook.dart
+++ lib/pages/SuggestBook.dart
@@ -2,17 +2,53 @@ import 'package:flutter/material.dart';
 import 'package:flutter_library/pages/widgets/InputField.dart';
 
 class SuggestBook extends StatelessWidget {
+  final _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      
       body: SafeArea(
         child: Column(
           children: [
-            InputFiled(hintText:'إسم الكتاب'),
-            InputFiled(hintText:'إسم الكاتب'),
-            InputFiled(hintText:'ملاحظة'),
+            Form(
+                key: _formKey,
+                child: Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: TextFormField(
 
+                        decoration: InputDecoration(labelText: 'First name'),
+
+                        validator: (value) {
+                          if (value.isNotEmpty) {
+                            if(validateEmail(value))
+                             return null;
+                            else
+                              return 'Email is not true ';
+                          }
+
+                          return 'Please enter some text';
+                        },
+                      ),
+                    ),
+                    ElevatedButton(
+                      onPressed: () {
+                        // Validate returns true if the form is valid, or false
+                        // otherwise.
+                        if (_formKey.currentState.validate()) {
+                          // If the form is valid, display a Snackbar.
+                          Scaffold.of(context).showSnackBar(
+                              SnackBar(content: Text('Processing Data')));
+                          print (
+                            'successfuly'
+                          );
+                        }
+                      },
+                      child: Text('Submit'),
+                    ),
+                  ],
+                ))
           ],
         ),
       ),
@@ -20,5 +56,9 @@ class SuggestBook extends StatelessWidget {
   }
 }
 
-
-
+bool validateEmail(String value) {
+  Pattern pattern =
+      r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+  RegExp regex = new RegExp(pattern);
+  return (!regex.hasMatch(value)) ? false : true;
+}
