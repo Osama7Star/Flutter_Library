cb606881eba49058a3b0759abfb66ae1eb0cda29
OsamaAlnajm <56093809+OsamaAlnajm@users.noreply.github.com>
2021-02-18 18:50:13 +0300

    6. Commit

--- lib/pages/SuggestBook.dart
+++ lib/pages/SuggestBook.dart
@@ -56,6 +56,8 @@ class SuggestBook extends StatelessWidget {
   }
 }
 
+
+// validate the email
 bool validateEmail(String value) {
   Pattern pattern =
       r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
